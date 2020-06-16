ba1bbafb18e31eea452273c21054aa586221648f
kaznak <kaznak.at.work@gmail.com>
2020-06-16 12:55:29 +0900

    next.config.js: add debuging toggle

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 // reference: https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+const util = require('util')
 
 module.exports = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   webpack: (config, { defaultLoaders }) => {
+    // console.log(util.inspect(config, { showHidden: false, depth: null }))
     config.module.rules.push({
       test: /\.mdx?$/,
       use: [defaultLoaders.babel, '@mdx-js/loader'],
