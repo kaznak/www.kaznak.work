7b5cdf721eb7b7cb3017bad2e1208eb34b6a6b40
kaznak <kaznak.at.work@gmail.com>
2020-06-18 21:18:22 +0900

    pages/_app.tsx: global meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,27 @@
+import * as params from '../lib/const'
+
+import Head from 'next/head'
 import '../styles/index.css'
 
-type MyAppArg = {
+interface MyAppArg {
   Component: any // !!TODO!! specify precise type
+  router: any // !!TODO!! specify precise type
   pageProps: Record<string, any>
 }
 
-export const MyApp = ({ Component, pageProps }: MyAppArg) => {
-  return <Component {...pageProps} />
+export const MyApp = (page: MyAppArg) => {
+  const { Component, router, pageProps } = page
+  return (
+    <>
+      <Head>
+        <link rel="canonical" href={params.URL_ORIGIN + router.pathname} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
 
 export default MyApp
