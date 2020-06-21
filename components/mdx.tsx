dd953cc45a5f61700b4b47dc0c842b40d7c32fc0
kaznak <kaznak.at.work@gmail.com>
2020-06-21 12:31:41 +0900

    components/mdx.tsx: add refrerence

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,3 +1,4 @@
 // see: https://mdxjs.com/advanced/components#updating-the-mapping-object-during-application-runtime
+// see: https://mdxjs.com/table-of-components#table-of-components
 
 export default {}
