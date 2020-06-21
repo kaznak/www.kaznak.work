9dae4f2bceb545c6d97787fca16f0b2e8f953e01
kaznak <kaznak.at.work@gmail.com>
2020-06-21 12:31:09 +0900

    lib/types.tsx: fix type declaration

--- lib/types.tsx
+++ lib/types.tsx
@@ -1,8 +1,8 @@
-import { AppProps as OrgAppProps } from 'next/app'
+import { AppProps as OrigAppProps } from 'next/app'
 import { NextComponentType, NextPageContext } from 'next/types/index'
 
-export interface AppProps extends OrgAppProps {
-  Component: NextComponentType<NextPageContext, any, {}> & {
+export declare type AppProps<P = {}> = OrigAppProps<P> & {
+  Component: NextComponentType<NextPageContext, any, P> & {
     isMDXComponent: boolean
   }
 }
