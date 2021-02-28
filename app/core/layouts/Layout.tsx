03a25873fa37cc7dde8add1725d0374fbf5a7ef7
kaznak <kaznak.at.work@gmail.com>
2021-02-28 18:19:22 +0900

    app/core: add cannonical url

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,5 +1,8 @@
 import { ReactNode } from "react"
 import { Head } from "blitz"
+import { useRouter } from "next/router"
+
+import { siteUrlOrigin } from "app/core/lib/constants"
 
 type LayoutProps = {
   title?: string
@@ -7,11 +10,15 @@ type LayoutProps = {
 }
 
 const Layout = ({ title, children }: LayoutProps) => {
+  const router = useRouter()
+
   return (
     <>
       <Head>
+        <meta name="robots" content="noindex, nofollow" />
         <title>{title || "www.kaznak.work"}</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href={siteUrlOrigin + router.pathname} />
       </Head>
 
       {children}
---
+++ app/core/lib/constants.ts
@@ -0,0 +1,1 @@
+export const siteUrlOrigin = process.env.VERCEL_URL || "https://www.example.com"
